43782d5120f069fd67b030ae1fb067ff75ca8d57
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2025-01-06 15:25:57 +0200

    Solution

---
+++ app/serializer.py
@@ -0,0 +1,20 @@
+import json
+from xml.etree import ElementTree
+
+from app.book import Book
+
+
+class BookSerializer:
+    @staticmethod
+    def serialize(book: Book, serialize_type: str) -> str:
+        if serialize_type == "json":
+            return json.dumps({"title": book.title, "content": book.content})
+        elif serialize_type == "xml":
+            root = ElementTree.Element("book")
+            title = ElementTree.SubElement(root, "title")
+            title.text = book.title
+            content = ElementTree.SubElement(root, "content")
+            content.text = book.content
+            return ElementTree.tostring(root, encoding="unicode")
+        else:
+            raise ValueError(f"Unknown serialize type: {serialize_type}")
